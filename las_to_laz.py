11bf305ceea424a73d52beda6c50e9641fbe98aa
rmichie <michie.ryan@deq.state.or.us>
2015-04-13 10:46:24 -0700

    updates

--- las_to_laz.py
+++ las_to_laz.py
@@ -1,7 +1,55 @@
+# This script 
+# 1). walks a directory looking for .las or .laz files.
+# 2). Outputs a csv file with the path to the files it finds
+# 3). Converts the file using LASzip 
+# 4). Deletes the .las or .laz file after conversion
+# 5). Logs process in the csv output so you can track progress.
+# By Ryan Michie
+
 from __future__ import print_function
 import csv
 import os
 import subprocess
+
+
+# inputs ##################################################
+walkpath = True
+exe_conversion = True
+
+# Extension/s to search for. This is always a tuple and
+# either .las, or .laz
+extensions_to_find = (".las")
+
+csv_las = r"\\DEQWQNAS01\Lidar06\las_paths_Lidar06.csv"
+
+# The top level directory or directories where the searching strarts. 
+# Always a list
+workspaces = [r"\\DEQWQNAS01\Lidar06"]
+#workspaces = [r"\\DEQWQNAS01\Lidar01",
+    #r"\\DEQWQNAS01\Lidar02",
+    #r"\\DEQWQNAS01\Lidar03",
+    #r"\\DEQWQNAS01\Lidar04",
+    #r"\\DEQWQNAS01\Lidar05",
+    #r"\\DEQWQNAS01\Lidar06"]
+
+# On windows you need to map server paths to a network drive letter :(
+pathletter_dict = {r'\\DEQWQNAS01\Lidar01': r'G:',
+                   r'\\DEQWQNAS01\Lidar02': r'H:',
+                   r'\\DEQWQNAS01\Lidar03': r'I:',
+                   r'\\DEQWQNAS01\Lidar04': r'J:',
+                   r'\\DEQWQNAS01\Lidar05': r'K:',
+                   r'\\DEQWQNAS01\Lidar06': r'L:',
+                   r'\\DEQWQNAS01\Lidar07': r'M:',
+                   r'\\DEQWQNAS01\Lidar08': r'N:'}
+
+# directories to ignore when walking
+ignore = ["REPORT", "VEC", "SHAP", "ASC", "TIN", "INTEN", "RASTER",
+          "BE", "BARE", "HH", "HIGH", "VH", "VEGHT", "DEN",
+          "HILLS","HL", "HILSD", "HS", "ORTHO", "PHOTO","TRAJ",
+          "RECYCLER", "System Volume Information","Lower_Columbia",
+          "USFS_Original","Yamhill_DEQ","Deschutes_from_USFS_old", "XXX"]
+
+# inputs ##################################################
 
 def read_csv(csvfile, skipheader = False):
     """Reads an input csv file and returns the header row as a list
@@ -18,27 +66,7 @@ def write_csv(csvlist, csvfile):
         writer = csv.writer(f)
         for row in csvlist:
             writer.writerow(row)
-
-def  build_raster_list_from_csv(csvfile, path_col, name_col):
-    """Read from a csv, and pull the path, year,
-    and raster file names of each raster to be processed into a sorted list.
-    csv must contain a fields titled Year, Path, and Name"""
-    csv_list = read_csv(csvfile, skipheader = True)
-    # Pull the path, year, and raster file name. 
-    # The second name is a placeholder for a new name when there are duplicates
-    raster_list = [[row[year_col],
-                  row[path_col],
-                  row[name_col],
-                  row[new_name_col]]for row in csv_list]
-    
-    # sort the list by year and then NewName
-    raster_list = sorted(raster_list, key=itemgetter(0, 3), reverse=False)        
-    
-    # Add a cols for processing status and the sort order
-    raster_list = [["#", n] + raster_list[n] for n in range(0, len(raster_list))]
-        
-    return(raster_list)
-
+            
 def execute_cmd(cmd_list, exe, shel):
     """Executes commands to the command prompt using subprocess module.
     Commands must be a string in a list"""
@@ -59,36 +87,6 @@ def execute_cmd(cmd_list, exe, shel):
     status = "X"
     return (status)
 
-# input parameters
-walkpath = False
-convrt_to_laz = True
-
-csv_las = r"\\DEQWQNAS01\Lidar06\las_paths_Lidar06.csv"
-workspaces = [r"\\DEQWQNAS01\Lidar06"]
-
-#workspaces = [r"\\DEQWQNAS01\Lidar01",
-    #r"\\DEQWQNAS01\Lidar02",
-    #r"\\DEQWQNAS01\Lidar03",
-    #r"\\DEQWQNAS01\Lidar04",
-    #r"\\DEQWQNAS01\Lidar05",
-    #r"\\DEQWQNAS01\Lidar06"]
-
-pathletter_dict = {r'\\DEQWQNAS01\Lidar01': r'G:',
-                   r'\\DEQWQNAS01\Lidar02': r'H:',
-                   r'\\DEQWQNAS01\Lidar03': r'I:',
-                   r'\\DEQWQNAS01\Lidar04': r'J:',
-                   r'\\DEQWQNAS01\Lidar05': r'K:',
-                   r'\\DEQWQNAS01\Lidar06': r'L:',
-                   r'\\DEQWQNAS01\Lidar07': r'M:',
-                   r'\\DEQWQNAS01\Lidar08': r'N:'}
-
-# directories to ignore when walking
-ignore = ["REPORT", "VEC", "SHAP", "ASC", "TIN", "INTEN", "RASTER",
-          "BE", "BARE", "HH", "HIGH", "VH", "VEGHT", "DEN",
-          "HILLS","HL", "HILSD", "HS", "ORTHO", "PHOTO","TRAJ",
-          "RECYCLER", "System Volume Information","Lower_Columbia",
-          "USFS_Original","Yamhill_DEQ","Deschutes_from_USFS_old", "XXX"]
-
 if walkpath is True:
     las_list = []
     print("starting to walk")
@@ -105,14 +103,14 @@ if walkpath is True:
                 dirnames.remove(r)            
             
             for f in filenames:
-                if f.endswith(".las"):
+                if f.endswith(extensions_to_find):
                     las_list.append(["#", f, os.path.join(dirpath, f)])
                     
     print("writing paths to csv")
     write_csv(las_list, csv_las)
     print("Done walking")
 
-if convrt_to_laz is True:
+if exe_conversion is True:
     n = 0
     las_list = read_csv(csv_las, skipheader=False)
     for row in las_list:
@@ -147,12 +145,12 @@ if convrt_to_laz is True:
                         else:
                             # update the csv file
                             las_list[n][0] = status
-                            write_csv(las_list, csv_las)                  
+                            write_csv(las_list, csv_las)
             else:
                 print("Error: " + inpath_temp + " does not exist")
                 status = "E"
                 las_list[n][0] = status
                 write_csv(las_list, csv_las)
         n = n + 1
-    print("Done converting to laz")
+    print("Done converting from {0}".format(extensions_to_find))
 print("Done with script")
