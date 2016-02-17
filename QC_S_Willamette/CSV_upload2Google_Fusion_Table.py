50246ac016c9ff95e829228c3fed278943187f42
rmichie <michie.ryan@deq.state.or.us>
2016-02-16 18:50:31 -0800

    updates

--- QC_S_Willamette/CSV_upload2Google_Fusion_Table.py
+++ QC_S_Willamette/CSV_upload2Google_Fusion_Table.py
@@ -1,5 +1,5 @@
 """
-Upload data from a csv file to a Fusion tables
+Upload data from a csv file to a Fusion table
 using the Google Fushion Tables API
 
 """
@@ -17,52 +17,7 @@ using the Google Fushion Tables API
 from __future__ import print_function
 import json
 import os
-
-# set the working directory
-#os.chdir(r"C:\WorkSpace\Quantifying_Conservation_2014\SouthernWillamette\Mapping_Web")
-os.chdir(r"/Users/rmichie/Desktop")
-
-api_file = "api_file.json"
-credentials_file = 'credentials.json'
-client_secret_file = 'client_secret_other.json'
-login_file = 'login_file.json'
-
-isExportable= "false"
-name="Test Table"
-attribution="Me myself and I" 
-attributionLink=None
-description="this is a test"
-
-# These are columns names
-col_list = [{"name": "ColName1",
-             "columnId": 0,
-             "type": "STRING"},
-            {"name": "ColName2",
-             "columnId": 1,
-             "type": "NUMBER"},
-            {"name": "LAT",
-             "columnId": 2,
-             "type": "LOCATION"}]
-
-table_data = {"columns": col_list,
-              "isExportable": isExportable,
-              "name": name,
-              "attribution": attribution,
-              "attributionLink": attributionLink,
-              "description": description
-            }
-
-# ----
-
-#import requests
-
-#payload = {"key": api_key}
-#r = requests.get(url = "https://www.googleapis.com/fusiontables/v2/tables", params= payload)
-#r = requests.post("https://www.googleapis.com/fusiontables/v2/tables",
-#                  params = payload,
-#                  data = table_data)
-#print(r.status_code, r.reason)
-
+import csv
 
 import httplib2
 
@@ -72,14 +27,37 @@ from oauth2client.file import Storage
 from oauth2client import client
 from oauth2client import tools
 import argparse
-import csv
 
+
+# set the working directory
+os.chdir(r"C:\WorkSpace\Quantifying_Conservation_2014\SouthernWillamette\Mapping_Web")
+#os.chdir(r"/Users/rmichie/Desktop")
+
+api_file = "api_file.json"
+credentials_file = 'credentials.json'
+client_secret_file = 'client_secret_other.json'
+login_file = 'login_file.json'
+csv_file = 'outputs_model_nodes_v1.csv'
+
+# Table metadata
+isExportable= "false"
+name="CWP Effective Shade v1"
+attribution="Oregon DEQ, CWP" 
+attributionLink=None
+description="CWP Southern Willamette pilot project. Effective shade model results for current condtion and site potential"
 
 with open(login_file) as f:    
     login_data = json.load(f)
     
 with open(api_file) as f:
     api_key = json.load(f)["api_key"]
+    
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -90,7 +68,7 @@ def get_credentials():
     Returns:
         Credentials, the obtained credential.
     """
-    credential_dir = os.path.join(os.getcwd(), '.credentials')
+    credential_dir = os.getcwd()
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir, credentials_file)
@@ -102,11 +80,44 @@ def get_credentials():
                                               scope='https://www.googleapis.com/auth/fusiontables',
                                               login_hint=login_data['email'])
         flow.user_agent = "Fushion Tables Python Script"
-        credentials = tools.run_flow(flow, store, flags)
+        credentials = tools.run_flow(flow, locker, flags)
 
     return credentials
 
+def format_columns():
+    '''Reads'''
+    
+    with open(csv_file) as f:
+        csvreader = csv.reader(f)
+        cols = csvreader.next()
+        data = csvreader.next()
+        
+        col_list = []
+        for n, c in enumerate(cols):
+            col_item = {"name":c}
+            col_item['columnId'] = n
+            
+            if c.upper() in ['LOCATION']:
+                col_item['type'] = 'LOCATION'
+            elif is_number(data[n]):
+                col_item['type'] = 'NUMBER'
+            else:
+                col_item['type'] = 'STRING'
+                
+            
+            col_list.append(col_item)
+            
+    return col_list
+               
 def insert_new_table():
+    
+    table_data = {"columns": col_list,
+                  "isExportable": isExportable,
+                  "name": name,
+                  "attribution": attribution,
+                  "attributionLink": attributionLink,
+                  "description": description
+                  }     
     
     # make a table request
     request = service.table()
@@ -118,23 +129,53 @@ def insert_new_table():
         if f_table['name'] == table_data['name']:
             # table exists
             return f_table['tableId']
-        else:
-            # Create table and get table ID
-            response = request.insert(body = table_data).execute()
-            return response['tableId']    
+
+    # Create table and get table ID
+    response = request.insert(body = table_data).execute()
+    return response['tableId']    
     
 def insert_new_rows():
     
-    # upload metadata
-    request = service.table()
+    media_data = MediaFileUpload(csv_file, mimetype='application/octet-stream', resumable=True)
+    request =  service.table().importRows(tableId=table_id, media_body=media_data,
+                                          isStrict=True, startLine=1,
+                                          encoding='auto-detect')
+    response = request.execute()
     
-    csv_data = MediaFileUpload(csvFileName,mimetype='application/octet-stream')
-    request =  service.table().importRows(tableId=table_id, media_body=csv_data,
-                                          startLine=1, encoding='auto-detect')
-    response = request.execute()    
+    status = response.getStatus()
     
-    response = request.importRows(table_id, uploadType='resumable').execute()
+    return response['numRowsReceived']
     
+
+def insert_style():
+        
+    style_body = {"name": "CWP_shade_diff",
+                  "markerOptions": {
+                      "iconStyler": {
+                          "columnName": "SHADE_DIFF",
+                          "kind": "fusiontables#buckets",
+                           "buckets": [
+                               {
+                                   "min": 0.66,
+                                   "icon": "small_red"},
+                               {
+                                   "min": 0.16,
+                                   "max": 0.65, 
+                                   "icon": "small_yellow"
+                                },
+                               {
+                                   "max": 0.15,
+                                   "icon": "small_green"
+                               }
+                               ]
+                       }
+                       }
+               }    
+    
+    request = service.style().insert(tableId=table_id, body=style_body)
+    response = request.execute() 
+    
+    return response['styleId']
 
 flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 
@@ -144,10 +185,16 @@ http = credentials.authorize(httplib2.Http())
 service = build(serviceName='fusiontables', version='v2', http=http)
  #               developerKey= api_key)
 
+col_list = format_columns()
 table_id = insert_new_table()
+print("Insert Table Table ID {0}".format(table_id))
+#numRowsReceived = insert_new_rows()
+#print("Insert Rows status: {0}. Number of rows uploaded {1}".format(return_status, numRowsReceived))
+style_id = insert_style()
+print("Insert Style. Style_ID {1}".format(style_id))
 
 
 
-print(response)
+print("done")
 
 
