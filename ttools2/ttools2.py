736e83565fd33e42deaa77ef8ca1a16aa033f40e
rmichie <michie.ryan@deq.state.or.us>
2016-07-25 08:03:08 -0700

    reworked classes

--- ttools2/ttools2.py
+++ ttools2/ttools2.py
@@ -1,47 +1,45 @@
 from __future__ import division
 from __future__ import print_function
-from collections import defaultdict
 
 class Blocks(object):
     """
-    A block is a collection of stream nodes that are contained
-    within a specfic coordinate extent (a bounding box). Organizing
-    stream nodes into blocks makes it easier and more effceint to process
-    a large number of stream nodes.
+    A block object is a specfic coordinate extent (a bounding box)
+    with a collection of stream nodes that are contained
+    within the extent.
     """
+     
     def __init__(self, **kwargs):
+        
+        attrs = ["id", "xmin", "ymin", "xmax", "ymax", "nodes"]
+        
+        for attr in attrs:
+            x = kwargs[attr] if attr in kwargs.keys() else None
+            setattr(self, attr, x)       
                 
-        self.xmin = xmin
-        self.ymin = ymin
-        self.xmax = xmax
-        self.ymax = ymax
-        # order 0 left, 1 bottom, 2 right, 3 top
-        self.block_extent = [xmin, ymin, xmax, ymax]
-        self.nodes = nodes
         
 class StreamReach(object):
     """
-    A stream reach is a collecton of stream nodes organized
-    in an upstream to downstream order.
+    A stream reach object is a collecton of stream nodes.
     """
+      
     def __init__(self, **kwargs):
+
+        attrs = ["nodes", "stream_km"]        
         
-        self.stream_id = stream_id
-        self.nodes = nodes            
-    
-    def nested_dict(self): 
-        """Build a nested dictionary"""
-        return defaultdict(nested_dict)
-            
-    def get_stream_nodes(self):
+        for attr in attrs:
+            x = kwargs[attr] if attr in kwargs.keys() else None
+            setattr(self, attr, x)
+         
+    def get_node_data(self):
         data = {}
         for attr in self.__slots:
             data[attr] = getattr(self,attr)
-            
         return data
-        
+                                    
     def make_nodes(self):
         print("making nodes")
+        
+        self.nodes = [1, 2, 3, 4]
         
     def sort_nodes(self):
         print("sort nodes")
@@ -58,62 +56,47 @@ class StreamNode(object):
     An individual stream segment defined as a stream node object.
     Stream nodes are a collection node attributes and methods to
     derive those attributes.
-    
     """
-    
+      
     def __init__(self, **kwargs):
         
-        __slots = ["node_id",
-                   "stream_km", 
-                   "point_x",
-                   "point_y",
-                   "elevation",
-                   "gradient", 
-                   "left", "right", "chanwid",
-                   "topo",
-                   "landcover", ]
+        attrs = ["node_id",
+                 "stream_id"
+                 "stream_km", 
+                 "point_x",
+                 "point_y",
+                 "elevation",
+                 "gradient", 
+                 "left", "right", "chanwid",
+                 "topo",
+                 "landcover"]
         
-        for attr in __slots:
+        for attr in attrs:
             x = kwargs[attr] if attr in kwargs.keys() else None
             setattr(self, attr, x)
-            self.__slots = __slots
-            self.__slots.sort()
-         
-    def get_node_data(self):
-        data = {}
-        for attr in self.__slots:
-            data[attr] = getattr(self,attr)
-        return data
-    
+             
     def measure_width(self):
         print("measureing channel width")
         
-        lb_distance = 3
-        rb_distance = 4
-        
-        return lb_distance, rb_distance
-        
+        self.lb_distance = 3
+        self.rb_distance = 4
+                
     def sample_elevation(self):
         print("get elevations")
         
-        z_node = 99
+        self.elevation = 99
         
-        return z_node
         
     def measure_topo_angles(self):
         print("get topo angles")
         
-        topo = {w: 33,
+        self.topo = {w: 33,
                 s: 21,
                 e: 9,}
-        
-        return topo
         
     def sample_landcover(self):
-        print("get landcover angles")
+        print("get landcover")
         
-        topo = {w: 33,
-                s: 21,
-                e: 9,}
-                
-        return topo    
+        self.landcover = {w: 1,
+                s: 2,
+                e: 3,}   
--- ttools2/ttools2_example.py
+++ ttools2/ttools2_example.py
@@ -1,65 +1,68 @@
 import imp
 import os
+from collections import defaultdict
 
 
-ttools2_path = r"C:\WorkSpace\GitHub\TTools\ttools2.py"
+#ttools2_path = r"C:\WorkSpace\GitHub\TTools\ttools2.py"
+
+ttools2_path = r"/Users/rmichie/Documents/GitHub/PyScripts/ttools2/ttools2.py"
 
 ttools = imp.load_source("ttools2", ttools2_path)
 
-
-def nested_dict(self): 
+def nested_dict(): 
     """Build a nested dictionary"""
-    return defaultdict(nested_dict)   
+    return defaultdict(nested_dict)
 
-def read_nodes_fc(nodes_fc, overwrite_data, addFields):
-    """Reads the input point feature class and returns the
-    STREAM_ID, NODE_ID, and X/Y coordinates as a nested dictionary"""
+def read_nodes_fc1(overwrite_data = True , addFields = None):
+    """Reads the input point feature class and returns the STREAM_ID, NODE_ID, and X/Y coordinates as a nested dictionary"""
     
-    nodeDict = nested_dict()
+    node = nested_dict()
+    stream = nested_dict()
+
     incursorFields = ["STREAM_ID","NODE_ID", "STREAM_KM", "SHAPE@X","SHAPE@Y"]
+    
+    arcobject = [["sid1", 1, 0.00, -122.3, 45.3 ],
+                 ["sid1", 2, 0.05, -122.3, 45.3 ],
+                 ["sid1", 3, 0.10, -122.3, 45.3 ],
+                 ["sid2", 4, 0.00, -122.3, 45.3 ],
+                 ["sid2", 5, 0.00, -122.3, 45.3 ],
+                 ]
 
-    # Get a list of existing fields
-    existingFields = []
-    for f in arcpy.ListFields(nodes_fc):
-        existingFields.append(f.name)     
-
-    # Check to see if the 1st field exists if yes add it.
-    if overwrite_data is False and (addFields[0] in existingFields) is True:
-        incursorFields.append(addFields[0])
+    if overwrite_data:
+        for row in arcobject:
+            stream1 = ttools.StreamReach(stream_id = row[0], 
+                                        node_id = row[1])
+            
+            stream[row[0]] = stream1
+        
+            node1 = ttools.StreamNode(node_id = row[1],
+                                     stream_km = row[2], 
+                                     point_x = row[3],
+                                     point_y = row[4])
+            node[row[1]] = node1
     else:
-        overwrite_data = True
-
-    # Determine input point spatial units
-    proj_nodes = arcpy.Describe(nodes_fc).spatialReference
-
-    with arcpy.da.SearchCursor(nodes_fc,incursorFields,"",proj_nodes) as Inrows:
-        if overwrite_data is True:
-            for row in Inrows:
-                StreamNode(node_id=row[1], 
-                           stream_km = row[2],
-                           point_x = row[3],
-                           point_y = row[4])
-        else:
-            for row in Inrows:
-                # if the data is null or zero (0 = default for shapefile),
-                # it is retreived and will be overwritten.
-                if row[5] is None or row[5] == 0 or row[5] < -9998:
-                    StreamNode(node_id=row[1], 
-                               stream_km = row[2],
-                               point_x = row[3],
-                               point_y = row[4])
-    if len(nodeDict) == 0:
+        for row in arcobject:
+            # if the data is null or zero (0 = default for shapefile),
+            # it is retreived and will be overwritten.
+            if row[5] is None or row[5] == 0 or row[5] < -9998:
+                stream1 = ttools.StreamReach(stream_id = row[0], 
+                                            node_id = row[1])
+                
+                stream[row[0]] = stream1
+            
+                node = ttools.StreamNode(node_id = row[1],
+                                         stream_km = row[2], 
+                                         point_x = row[3],
+                                         point_y = row[4])
+                node[row[1]] = node1
+                    
+    if len(node) == 0:
         sys.exit("The fields checked in the input point feature class "+
                  "have existing data. There is nothing to process. Exiting")
             
-    return(nodeDict)
+    return(stream, node)
 
 
-node = ttools.StreamNode()
-block = ttools.Blocks()
-
-
-
-
+stream, node = read_nodes_fc1()
 print("blah")
 
